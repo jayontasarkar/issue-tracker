cc47b5b251021a667e4ea897b0fe4f9a4c23e891
Jayonta Sarkar <jsarkar@awesomemotive.com>
2023-11-17 11:06:51 +0600

    refactor skeleton component

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,6 @@
 import IssueActions from '@/components/shared/issues/IssueActions';
+import Skeleton from '@/components/ui/Skeleton';
 import { Table } from '@radix-ui/themes';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5, 6];
